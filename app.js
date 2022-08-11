44a5618701e3d7226987b9609e609d55aee79706
Denver <70090559+Denver44@users.noreply.github.com>
2022-08-11 19:14:17 +0530

    second commit

--- app.js
+++ app.js
@@ -1,24 +1,9 @@
 import express from 'express';
-import Lead from './models/leadsModel.js';
 let app = express();
 
+import LeadRouter from './routers/LeadRouter.js';
+
 app.use(express.json());
-
-app.post('/leads', async (req, res) => {
-  try {
-    const newDoc = await Lead.create(req.body);
-
-    res.status(201).json({
-      status: 'created',
-      data: {
-        data: newDoc,
-      },
-    });
-  } catch (error) {
-    res.status(400).json({
-      error,
-    });
-  }
-});
+app.use(LeadRouter);
 
 export default app;
