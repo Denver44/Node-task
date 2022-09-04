61eb1fed20643f86b429ea023e5eda45da367e57
Denver <70090559+Denver44@users.noreply.github.com>
2022-09-05 00:56:04 +0530

    App ready

--- routers/LeadRouter.js
+++ routers/LeadRouter.js
@@ -53,13 +53,9 @@ const router = new express.Router();
  *           description: The wake up time
  *           example: "8:00PM"
  *         sleepHours:
- *           type: string
+ *           type: Number
  *           description: The number of hours you sleep
- *           example: "7"
- *         createdAt:
- *           type: string
- *           format: date
- *           description: The date of the record creation
+ *           example: 7
  */
 
 /**
@@ -71,7 +67,7 @@ const router = new express.Router();
 
 /**
  * @swagger
- * /leads:
+ * /api/v1/leads:
  *   get:
  *     summary: Returns the list of all the leads
  *     tags: [Leads]
@@ -84,12 +80,15 @@ const router = new express.Router();
  *               type: array
  *               items:
  *                 $ref: '#/components/schemas/Lead'
+ *       500:
+ *         description: Internal Server Error
  */
+
 router.get('/', getAllLeads);
 
 /**
  * @swagger
- * /leads/{id}:
+ * /api/v1/leads/{id}:
  *   get:
  *     summary: Get the lead by id
  *     tags: [Leads]
@@ -109,13 +108,15 @@ router.get('/', getAllLeads);
  *               $ref: '#/components/schemas/Lead'
  *       404:
  *         description: The lead was not found
+ *       500:
+ *         description: Internal Server Error
  */
 
 router.get('/:id', getLeadByID);
 
 /**
  * @swagger
- * /leads:
+ * /api/v1/leads:
  *   post:
  *     summary: Create a new lead
  *     tags: [Leads]
@@ -127,16 +128,52 @@ router.get('/:id', getLeadByID);
  *             $ref: '#/components/schemas/Lead'
  *     responses:
  *       200:
- *         description: The Lead was successfully created
+ *         description: The lead was successfully created
  *         content:
  *           application/json:
  *             schema:
  *               $ref: '#/components/schemas/Lead'
+ *       400:
+ *         description: Bad Request
  *       500:
  *         description: Some server error
  */
 
 router.post('/', createLead);
+
+/**
+ * @swagger
+ * /api/v1/leads/{id}:
+ *  patch:
+ *    summary: Update the book by the id
+ *    tags: [Leads]
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: string
+ *        required: true
+ *        description: The lead id
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/Lead'
+ *    responses:
+ *      200:
+ *        description: The lead was updated
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Lead'
+ *      400:
+ *        description: Bad Request
+ *      404:
+ *        description: The Lead was not found
+ *      500:
+ *        description: Internal Server Error
+ */
 
 router.patch('/:id', updateLeadById);
 
